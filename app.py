dcf89444b98055f8ddfbd362ce4b5428d6db9536
Manahil Ishfaq <ManahilIshfaq18@users.noreply.huggingface.co>
2026-04-25 04:53:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 # Load environment variables
 load_dotenv()
