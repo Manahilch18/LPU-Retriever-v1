49a4339841170146664c93de297f7da314f437c0
Manahil Ishfaq <ManahilIshfaq18@users.noreply.huggingface.co>
2026-04-25 05:12:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from langchain_community.document_loaders import PyPDFLoader, TextLoader, Direct
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
